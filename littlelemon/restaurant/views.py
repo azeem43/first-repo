2614a0b56eed942fe9ceafe016f1b104b9b7fc7f
biodun <biodunsaheed20199@gmail.com>
2023-04-05 07:44:38 -0700

    message

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from . models import Menu,Booking
+from . serializers import MenuSerializer,BookingSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html',{})
+
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+    
+class BookingView(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
